2c756a03e31ca3b678867016030c0a63f24cac6d
alensaito1 <erwinsmithscout1@gmail.com>
2021-09-30 21:08:46 +0530

    Typings!

---
+++ src/typings/Client.ts
@@ -0,0 +1,38 @@
+/** Whatsapp User ID */
+export type JID = `${string}@s.whatsapp.net`
+
+/** Whatsapp Group ID */
+export type GID = `${string}-${string}@g.us`
+
+export interface IClientConfig {
+    /**
+     * The session of the bot.
+     * @type {string}
+     * @memberof IConfig
+     */
+    session: string
+
+    /**
+     * The Prefix of the bot.
+     * @type {string}
+     * @memberof IConfig
+     * @default '!'
+     */
+    prefix: string
+
+    /**
+     * The token of the bot.
+     * @type {string}
+     * @memberof IConfig
+     * @default []
+     */
+    mods: (JID | string)[]
+}
+
+export interface ISession {
+    clientID: string
+    serverToken: string
+    clientToken: string
+    encKey: string
+    macKey: string
+}
