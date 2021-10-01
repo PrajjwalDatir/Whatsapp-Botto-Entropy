f9134db9bc3a8bde8bf650959fed6c19c1aa5fc2
alensaito1 <erwinsmithscout1@gmail.com>
2021-10-01 13:28:37 +0530

    `remove utils`

--- src/utils.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Checks if the given value is truthy.
- * @param value
- */
-export const isTruthy = <T>(value: T | null | undefined): value is T => {
-    return value !== null && value !== undefined
-}
