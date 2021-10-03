01a57db1fd63a5f076be1dc2630ffaaf089bf8d7
alensaito1 <erwinsmithscout1@gmail.com>
2021-10-03 19:21:49 +0530

    Working!

--- src/bot.ts
+++ src/bot.ts
@@ -1,10 +1,13 @@
 import Client from './Structures/Client'
 import getConfig from './getConfig'
 import Database from './Structures/Database'
-import { Server } from './Structures/Server'
 import { MessageHandler } from './Handlers/MessageHandler'
 import { join } from 'path'
+import P from 'pino'
+import { getAuthInfo, saveAuthInfo } from './authInfo'
+import { initInMemoryKeyStore } from '@adiwajshing/baileys'
 import Message from './Structures/Message'
+import { Server } from './Structures/Server'
 ;(async () => {
     if (!process.env.MONGO_URI) {
         console.error('No MONGO_URI found!')
@@ -13,34 +16,39 @@ import Message from './Structures/Message'
 
     const database = new Database(process.env.MONGO_URI)
     await database.connect()
-    const client = new Client(getConfig, database)
-    const messageHandler = new MessageHandler(join(__dirname, 'Commands'), client)
+    const config = getConfig()
 
+    const save = async (state: unknown) => await saveAuthInfo(database, config.session, state)
+
+    const { creds, keys } = (await getAuthInfo(database, config.session)) || {}
+
+    const client = new Client(config, database, {
+        printQRInTerminal: true,
+        logger: P({
+            level: 'fatal'
+        })
+    })
+
+    if (creds && keys)
+        client.loadAuth({
+            creds,
+            keys: initInMemoryKeyStore(keys)
+        })
+
+    client.on('auth-state.update', () => {
+        client.loadAuth(client.authState)
+        save(client.authState)
+    })
+    const messageHandler = new MessageHandler(join(__dirname, 'Commands'), client)
     messageHandler.loadCommands()
 
-    client.once('open', async () => {
-        client.log('Connected to Whatsapp')
-        console.table(client.user)
-        await client.saveAuthInfo(client.base64EncodedAuthInfo())
-    })
     const server = new Server(client)
+    const PORT = process.env.PORT || 3000
+    server.listen(PORT, () => client.log(`Server Listening on port ${PORT}`))
 
-    server.listen(process.env.PORT || 3000, () => console.log(`Listening on port ${process.env.PORT || 3000}`))
-
-    client.on('new-message', (M: Message) => {
+    client.on('new.message', (M: Message) => {
         messageHandler.handle(M)
     })
 
-    client.on('close', async ({ isReconnecting }) => {
-        client.log('Disconnected from Whatsapp')
-        client.log('Attempting to Reconnect')
-        client.once('open', () => {
-            client.log('Reconnected to Whatsapp')
-        })
-        if (!isReconnecting) await client.connect()
-    })
-
-    const authinfo = await client.getAuthInfo()
-    if (authinfo) client.loadAuthInfo(authinfo)
     await client.connect()
 })()
